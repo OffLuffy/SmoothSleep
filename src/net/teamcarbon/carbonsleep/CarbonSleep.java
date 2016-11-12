b3c7dabd4f8f11637c1a2c18920b2ad0736ff1e6
OffLuffy <lutherlangford@gmail.com>
2016-11-12 14:25:52 -0500

    Fixed indents

--- src/net/teamcarbon/carbonsleep/CarbonSleep.java
+++ src/net/teamcarbon/carbonsleep/CarbonSleep.java
@@ -24,8 +24,8 @@ public class CarbonSleep extends CarbonPlugin {
 
 	public void enablePlugin() {
 
-        pm().registerEvents(new PlayerEventsListener(), this);
-        pm().registerEvents(new MorningEventListener(), this);
+		pm().registerEvents(new PlayerEventsListener(), this);
+		pm().registerEvents(new MorningEventListener(), this);
 
 		List<String> disabledWorlds = getConf().getStringList("disabled-worlds");
 		for (World w : server().getWorlds()) {
@@ -39,34 +39,34 @@ public class CarbonSleep extends CarbonPlugin {
 			public void run() {
 				if (nightTimes.isEmpty()) return;
 				for (World w : nightTimes.keySet()) {
-                    if (w.getEnvironment() != Environment.NORMAL) { nightTimes.remove(w); continue; }
-                    if (w.getTime() < SLEEP_TICKS_START || w.getTime() > SLEEP_TICKS_END) { continue; }
+					if (w.getEnvironment() != Environment.NORMAL) { nightTimes.remove(w); continue; }
+					if (w.getTime() < SLEEP_TICKS_START || w.getTime() > SLEEP_TICKS_END) { continue; }
 
-                    // Calculate some math for time and stuff
-                    long duraTicks = nightTimes.get(w) * 20; // Number of ticks for night to last with 100% speed
-                    long ticksPerTick = SLEEP_TICKS_DURA / duraTicks - 1; // Ticks to pass per tick at 100% speed
+					// Calculate some math for time and stuff
+					long duraTicks = nightTimes.get(w) * 20; // Number of ticks for night to last with 100% speed
+					long ticksPerTick = SLEEP_TICKS_DURA / duraTicks - 1; // Ticks to pass per tick at 100% speed
 
-                    // Count players, track awake/asleep, ignored or not.
-                    int asleep = 0, awake = 0, ignoreAsleep = 0;
-                    List<Player> sleepers = new ArrayList<Player>();
-                    for (Player p : w.getPlayers()) {
-                        if (p.isSleeping()) sleepers.add(p);
-                    	if (p.isSleepingIgnored()) { if (p.isSleeping()) ignoreAsleep++; }
-                    	else { if (p.isSleeping()) asleep++; else awake++; }
-                    }
-                    int totalAsleep = asleep + ignoreAsleep;
+					// Count players, track awake/asleep, ignored or not.
+					int asleep = 0, awake = 0, ignoreAsleep = 0;
+					List<Player> sleepers = new ArrayList<>();
+					for (Player p : w.getPlayers()) {
+						if (p.isSleeping()) sleepers.add(p);
+						if (p.isSleepingIgnored()) { if (p.isSleeping()) ignoreAsleep++; }
+						else { if (p.isSleeping()) asleep++; else awake++; }
+					}
+					int totalAsleep = asleep + ignoreAsleep;
 
-                    if (totalAsleep == 0) continue; // Continue, no one is sleeping
+					if (totalAsleep == 0) continue; // Continue, no one is sleeping
 
-                    // Scale the 100% night speed down by the ratio of awake/asleep players
-                    ticksPerTick = NumUtils.remapValue(true, 0, ticksPerTick, 0, awake, totalAsleep);
+					// Scale the 100% night speed down by the ratio of awake/asleep players
+					ticksPerTick = NumUtils.remapValue(true, 0, ticksPerTick, 0, awake, totalAsleep);
 
-                    long newTime = w.getTime() + ticksPerTick;
-                    if (newTime > SLEEP_TICKS_END) newTime = SLEEP_TICKS_END;
+					long newTime = w.getTime() + ticksPerTick;
+					if (newTime > SLEEP_TICKS_END) newTime = SLEEP_TICKS_END;
 
-                    w.setTime(newTime);
+					w.setTime(newTime);
 
-                    if (newTime >= SLEEP_TICKS_END) { pm().callEvent(new MorningEvent(w, sleepers)); }
+					if (newTime >= SLEEP_TICKS_END) { pm().callEvent(new MorningEvent(w, sleepers)); }
 
 				}
 			}
@@ -75,16 +75,16 @@ public class CarbonSleep extends CarbonPlugin {
 
 	public static boolean worldEnabled(World w) { return nightTimes.containsKey(w); }
 
-    public static int getSleeperCount(World w) {
-        int s = 0;
-        for (Player p : w.getPlayers()) { if (p.isSleeping()) s++; }
-        return s;
-    }
+	public static int getSleeperCount(World w) {
+		int s = 0;
+		for (Player p : w.getPlayers()) { if (p.isSleeping()) s++; }
+		return s;
+	}
 
-    public static int getWakerCount(World w) {
-        int a = 0;
-        for (Player p : w.getPlayers()) { if (p.isSleepingIgnored() || !p.isSleeping()) a++; }
-        return a;
-    }
+	public static int getWakerCount(World w) {
+		int a = 0;
+		for (Player p : w.getPlayers()) { if (p.isSleepingIgnored() || !p.isSleeping()) a++; }
+		return a;
+	}
 
 }
